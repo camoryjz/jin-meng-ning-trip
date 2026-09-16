0c4ebf7dd12e639885355e257b25a820cf3abf54
camoryjz <camoryjz@gmail.com>
2026-09-16 23:33:09 +0800

    Load header separation fix and refresh terminal assets

--- public/trip-title.js
+++ public/trip-title.js
@@ -1,16 +1,20 @@
 (() => {
   const stylesheet = document.createElement("link");
   stylesheet.rel = "stylesheet";
-  stylesheet.href = "trip-terminal.css?v=20260916-1";
+  stylesheet.href = "trip-terminal.css?v=20260916-2";
   document.head.append(stylesheet);
 
   const terminalScript = document.createElement("script");
-  terminalScript.src = "trip-terminal.js?v=20260916-1";
+  terminalScript.src = "trip-terminal.js?v=20260916-2";
   document.head.append(terminalScript);
 
   const mapEnhancements = document.createElement("script");
   mapEnhancements.src = "trip-map-enhancements.js?v=20260916-1";
   document.head.append(mapEnhancements);
+
+  const headerFix = document.createElement("script");
+  headerFix.src = "header-order-fix.js?v=20260916-1";
+  document.head.append(headerFix);
 
   document.addEventListener("travel-data-ready", (event) => {
     const data = event.detail;
